72732a565950453aac6f99572d20d17f46e1c564
Penyqiu <kpenczynski@gmail.com>
2021-11-07 18:16:18 +0100

    Added basic database connections and viewmodel implementation

---
+++ app/src/main/java/com/example/shoppingapp/data/repositories/ShoppingRepository.kt
@@ -0,0 +1,14 @@
+package com.example.shoppingapp.data.repositories
+
+import com.example.shoppingapp.data.ShoppingDatabase
+import com.example.shoppingapp.data.entities.ShoppingItem
+
+class ShoppingRepository(
+    private val db: ShoppingDatabase
+) {
+    suspend fun upsert(item: ShoppingItem) = db.getShoppingDao().upsert(item)
+
+    suspend fun delete(item: ShoppingItem) = db.getShoppingDao().delete(item)
+
+    fun getAllShoppingItems() = db.getShoppingDao().getAllShoppingItems()
+}
