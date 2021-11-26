25b0cac4c5a19cf7be2e4e731e6593556d2a7d6d
Penyqiu <kpenczynski@gmail.com>
2021-11-26 22:02:33 +0100

    Added drawer

--- app/src/main/java/com/example/shoppingapp/ui/ShoppingActivity.kt
+++ app/src/main/java/com/example/shoppingapp/ui/ShoppingActivity.kt
@@ -1,6 +1,9 @@
 package com.example.shoppingapp.ui
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -17,6 +20,7 @@ class ShoppingActivity : AppCompatActivity(), KodeinAware {
 
     override val kodein by kodein()
     private val factory: ShoppingViewModelFactory by instance()
+    lateinit var toggle : ActionBarDrawerToggle
 
     lateinit var viewModel: ShoppingViewModel
 
@@ -24,12 +28,27 @@ class ShoppingActivity : AppCompatActivity(), KodeinAware {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping)
 
+        toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
         viewModel = ViewModelProvider(this, factory).get(ShoppingViewModel::class.java)
 
         val adapter = ShoppingItemAdapter(listOf(), viewModel)
 
         rvShoppingItems.layoutManager = LinearLayoutManager(this)
         rvShoppingItems.adapter = adapter
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId)
+            {
+                R.id.Glowna->Toast.makeText(applicationContext,"Powrot do glowenej",Toast.LENGTH_SHORT).show()
+                R.id.Mapy->Toast.makeText(applicationContext,"Mapy",Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
 
         viewModel.getAllShoppingItems().observe(this, Observer {
             adapter.items = it
@@ -42,9 +61,17 @@ class ShoppingActivity : AppCompatActivity(), KodeinAware {
                 object : AddDialogListener {
                     override fun onAddButtonClicked(item: ShoppingItem) {
                         viewModel.upsert(item)
-                    }
-                }).show()
+                  }
+              }).show()
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item))
+        {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
